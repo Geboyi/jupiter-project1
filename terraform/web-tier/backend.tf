139a902a29ec69e438a68a36030bd46ada2193ab
techoutcomes <127057485+techoutcomes@users.noreply.github.com>
2023-09-10 15:24:11 +0100

    Update backend.tf

--- terraform/web-tier/backend.tf
+++ terraform/web-tier/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "technologiesoutcomes-terraform-backend"
     encrypt = true
-    key     = "3tier-web.tfstate"
+    key     = "jupiter/3tier-web.tfstate"
     region  = "eu-west-1"
     #dynamodb_table = "technologiesoutcomes-terraform-backend"
   }
