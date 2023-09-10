0db332dcdd42a9e80d0d19980321e2f6e89f4474
victor.katte <victor.katte@totalcloudconnect.com>
2023-09-10 22:21:01 +0100

    cleanup

--- application/web-tier/ec2/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "technologiesoutcomes-terraform-backend"
-    encrypt = true
-    key     = "3tier-web.tfstate"
-    region  = "eu-west-1"
-    #dynamodb_table = "technologiesoutcomes-terraform-backend"
-  }
-}
